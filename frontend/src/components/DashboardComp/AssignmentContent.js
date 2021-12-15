2c44dbcdb5973fac75efe50a591e063de00d287b
kaydayo <olukayodedayo100@gmail.com>
2021-12-15 20:21:16 +0100

    changed to html css and js

--- frontend/src/components/DashboardComp/AssignmentContent.js
+++
@@ -1,9 +0,0 @@
-export default function AssignmentContent() {
-  return (
-    <div>
-      <textarea>This is a sticky note you can type and edit.</textarea>
-
-      <div id='create'>+</div>
-    </div>
-  )
-}
